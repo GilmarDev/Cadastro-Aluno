f8b2b33843fb9f733f934bfc53d7261d6da652e4
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-09-29 20:55:11 -0300

    Primeira classe e objeto aluno

---
+++ src/cursojava/classes/Aluno.java
@@ -0,0 +1,17 @@
+package cursojava.classes;
+
+/*Esta é a classe/objeto que representa Aluno*/
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+}
---
+++ src/pacote/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package pacote.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*main é um método em Java auto executável*/
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
