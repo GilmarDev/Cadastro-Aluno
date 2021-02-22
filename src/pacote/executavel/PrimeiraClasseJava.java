310e0974bb02ffc5c57233a25a8fc928cae2ac08
Gilmar Cruz Silva <gilmarcs1@hotmail.com>
2021-02-21 21:29:05 -0300

    capturando exceções não tratada

--- src/pacote/executavel/PrimeiraClasseJava.java
+++ src/pacote/executavel/PrimeiraClasseJava.java
@@ -1,17 +1,17 @@
 package pacote.executavel;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 import classesauxiliares.FuncaoAutentificacao;
-import curso.java.interfaces.PermitirAcesso;
 import cursojava.classes.Aluno;
 import cursojava.classes.Diretor;
 import cursojava.classes.Disciplina;
-import cursojava.classes.Secretario;
 import cursojava.constantes.StatusAluno;
 
 public class PrimeiraClasseJava {
@@ -20,114 +20,144 @@ public class PrimeiraClasseJava {
 	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) {
 
-		String login = JOptionPane.showInputDialog("Informe o login");
-		String senha = JOptionPane.showInputDialog("Informe a senha");
+		try {
 
-		
-		if (new FuncaoAutentificacao(new Diretor(login, senha)).autenticar()) {/* se TRUE acessa se False não acessa */
+			File file = new File("arquivo.txt");
+			Scanner scanner = new Scanner(file);
 
-			List<Aluno> alunos = new ArrayList<Aluno>();
+			String login = JOptionPane.showInputDialog("Informe o login");
+			String senha = JOptionPane.showInputDialog("Informe a senha");
 
-			/*
-			 * é uma lista que dentro dela temos uma chave que identifica uma sequencia de
-			 * valores
-			 */
-			HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
+			if (new FuncaoAutentificacao(new Diretor(login, senha))
+					.autenticar()) {/* se TRUE acessa se False não acessa */
 
-			for (int qtd = 1; qtd <= 5; qtd++) {
-
-				String nome = JOptionPane.showInputDialog("Qual o nome do aluno " + qtd + " ?");
+				List<Aluno> alunos = new ArrayList<Aluno>();
 				/*
-				 * String idade = JOptionPane.showInputDialog("Qual é a idade? "); String
-				 * dataNascimento = JOptionPane.showInputDialog("Data de nascimento? "); String
-				 * rg = JOptionPane.showInputDialog("Registro Geral? "); String cpf =
-				 * JOptionPane.showInputDialog("Qual número do CPF? "); String mae =
-				 * JOptionPane.showInputDialog("Nome da mãe? "); String pai =
-				 * JOptionPane.showInputDialog("Nome do pai? "); String matricula =
-				 * JOptionPane.showInputDialog("Qual data da matrícula? "); String serie =
-				 * JOptionPane.showInputDialog("Qual é a série? "); String escola =
-				 * JOptionPane.showInputDialog("nome da escola? ");/*
-				 * 
-				 * /* new Aluno() é uma instância (Criação do objeto)
+				 * é uma lista que dentro dela temos uma chave que identifica uma sequencia de
+				 * valores
 				 */
-				/* aluno é uma referência para objeto aluno */
+				HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
 
-				Aluno aluno = new Aluno();
+				for (int qtd = 1; qtd <= 1; qtd++) {
 
-				aluno.setNome(nome);
-				/*
-				 * aluno.setIdade(Integer.valueOf(idade));
-				 * aluno.setDataNascimento(dataNascimento); aluno.setRegistroGeral(rg);
-				 * aluno.setNumeroCpf(cpf); aluno.setNomeMae(mae); aluno.setNomePai(pai);
-				 * aluno.setDataMatricula(matricula); aluno.setSerieMatriculado(serie);
-				 * aluno.setNomeEscola(escola);
-				 */
+					String nome = JOptionPane.showInputDialog("Qual o nome do aluno " + qtd + " ?");
+					String idade = JOptionPane.showInputDialog("Qual é a idade? "); 
+				    
+					/*String dataNascimento = JOptionPane.showInputDialog("Data de nascimento? "); 
+					String rg = JOptionPane.showInputDialog("Registro Geral? "); 
+					String cpf = JOptionPane.showInputDialog("Qual número do CPF? "); 
+					String mae = JOptionPane.showInputDialog("Nome da mãe? ");
+					String pai = JOptionPane.showInputDialog("Nome do pai? "); 
+					String matricula = JOptionPane.showInputDialog("Qual data da matrícula? "); 
+				    String serie = JOptionPane.showInputDialog("Qual é a série? "); 
+					String escola = JOptionPane.showInputDialog("nome da escola? ");
+													 
+					 new Aluno() {} //é uma instância (Criação do objeto)*/
+													 
+					//aluno é uma referência para objeto aluno 
+					Aluno aluno = new Aluno();
 
-				for (int pos = 1; pos <= 1; pos++) {
-					String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + pos + " ?");
-					String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + pos + " ?");
+					aluno.setNome(nome);
+					
+					  aluno.setIdade(Integer.valueOf(idade));
+					 /* aluno.setDataNascimento(dataNascimento); aluno.setRegistroGeral(rg);
+					 * aluno.setNumeroCpf(cpf); aluno.setNomeMae(mae); aluno.setNomePai(pai);
+					 * aluno.setDataMatricula(matricula); aluno.setSerieMatriculado(serie);
+					 * aluno.setNomeEscola(escola);
+					 */
 
-					Disciplina disciplina = new Disciplina();
-					disciplina.setDisciplina(nomeDisciplina);
-					disciplina.setNota(Double.valueOf(notaDisciplina));
+					for (int pos = 1; pos <= 1; pos++) {
+						String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + pos + " ?");
+						String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + pos + " ?");
 
-					aluno.getDisciplina().add(disciplina);
-				}
+						Disciplina disciplina = new Disciplina();
+						disciplina.setDisciplina(nomeDisciplina);
+						disciplina.setNota(Double.valueOf(notaDisciplina));
 
-				int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina ?");
-
-				if (escolha == 0) {/* Opção SIM e Zero */
-
-					int continuarRemover = 0;
-					int posicao = 1;
-
-					while (continuarRemover == 0) {
-						String disciplinaRemover = JOptionPane.showInputDialog("Qual a disciplina 1, 2, 3, ou 4 ?");
-						aluno.getDisciplina().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
-						posicao++;
-						continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover ?");
+						aluno.getDisciplina().add(disciplina);
 					}
+
+					int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina ?");
+
+					if (escolha == 0) {/* Opção SIM e Zero */
+
+						int continuarRemover = 0;
+						int posicao = 1;
+
+						while (continuarRemover == 0) {
+							String disciplinaRemover = JOptionPane.showInputDialog("Qual a disciplina 1, 2, 3, ou 4 ?");
+							aluno.getDisciplina().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+							posicao++;
+							continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover ?");
+						}
+					}
+					alunos.add(aluno);
 				}
 
-				alunos.add(aluno);
-			}
+				maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
+				maps.put(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
+				maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
 
-			maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
-			maps.put(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
-			maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
+				for (Aluno aluno : alunos) {
 
-			for (Aluno aluno : alunos) {
+					if (aluno.getAlunoAprovado().equalsIgnoreCase(StatusAluno.APROVADO)) {
+						maps.get(StatusAluno.APROVADO).add(aluno);
+					} else if (aluno.getAlunoAprovado().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
+						maps.get(StatusAluno.RECUPERACAO).add(aluno);
+					} else if (aluno.getAlunoAprovado().equalsIgnoreCase(StatusAluno.REPROVADO)) {
+						maps.get(StatusAluno.REPROVADO).add(aluno);
+					}
 
-				if (aluno.getAlunoAprovado().equalsIgnoreCase(StatusAluno.APROVADO)) {
-					maps.get(StatusAluno.APROVADO).add(aluno);
-				} else if (aluno.getAlunoAprovado().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
-					maps.get(StatusAluno.RECUPERACAO).add(aluno);
-				} else if (aluno.getAlunoAprovado().equalsIgnoreCase(StatusAluno.REPROVADO)) {
-					maps.get(StatusAluno.REPROVADO).add(aluno);
 				}
 
+				System.out.println("-------------------lista dos Aprovados-------------------------");
+				for (Aluno aluno : maps.get(StatusAluno.APROVADO)) {
+					System.out.println("O aluno " + aluno.getNome() + " teve resultado = " + aluno.getAlunoAprovado()
+							+ " a média foi : " + aluno.getMediaNota());
+
+				}
+				System.out.println("-------------------lista de Recuperação-------------------------");
+				for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
+					System.out.println("O aluno " + aluno.getNome() + " teve resultado = " + aluno.getAlunoAprovado()
+							+ " a média foi : " + aluno.getMediaNota());
+				}
+				System.out.println("-------------------lista dos Reprovados-------------------------");
+				for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
+					System.out.println("O aluno " + aluno.getNome() + " teve resultado = " + aluno.getAlunoAprovado()
+							+ " a média foi : " + aluno.getMediaNota());
+
+				}
+
+			} else {
+				JOptionPane.showMessageDialog(null, "Acesso não permitido ");
 			}
 
-			System.out.println("-------------------lista dos Aprovados-------------------------");
-			for (Aluno aluno : maps.get(StatusAluno.APROVADO)) {
-				System.out.println("O aluno " + aluno.getNome() + " teve resultado = " + aluno.getAlunoAprovado()
-						+ " a média foi : " + aluno.getMediaNota());
+		} catch (NumberFormatException e) {
 
-			}
-			System.out.println("-------------------lista de Recuperação-------------------------");
-			for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
-				System.out.println("O aluno " + aluno.getNome() + " teve resultado = " + aluno.getAlunoAprovado()
-						+ " a média foi : " + aluno.getMediaNota());
-			}
-			System.out.println("-------------------lista dos Reprovados-------------------------");
-			for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
-				System.out.println("O aluno " + aluno.getNome() + " teve resultado = " + aluno.getAlunoAprovado()
-						+ " a média foi : " + aluno.getMediaNota());
+			StringBuilder saida = new StringBuilder();
 
+			/* Imprimir erro no console Java */
+			e.printStackTrace();
+
+			/* Mensagem de erro ou causa */
+			System.out.println(" Mensagem " + e.getMessage());
+
+			for (int pos = 0; pos < e.getStackTrace().length; pos++) {
+
+				saida.append("\n Classe de erro " + e.getStackTrace()[pos].getClassName());
+				saida.append("\n Método de erro " + e.getStackTrace()[pos].getMethodName());
+				saida.append("\n Linha de erro " + e.getStackTrace()[pos].getLineNumber());
+				saida.append("\n Class : " + e.getClass().getName());
 			}
 
-		} else {
-			JOptionPane.showConfirmDialog(null, "Acesso não permitido ");
+			JOptionPane.showMessageDialog(null, "Erro conversão de número " + saida.toString());
+			
+		}catch (NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Opaa! um null pointer exeption : " + e.getClass());
+		}catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro inesperado " + e.getClass().getName());
 		}
 	}
+
 }
